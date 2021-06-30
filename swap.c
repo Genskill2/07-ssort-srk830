2579a3784c257aa334f6abb89a9d06c4c2d57d38
srk830 <84616986+srk830@users.noreply.github.com>
2021-06-30 21:53:47 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,11 +1,12 @@
 void swap_max(int a[], int l, int n) {
   int max = a[n];
   int max_pos = n;
-  for(int i = n+1; i < l; i++) {
+  int i = n+1;
+  while(i < l) {
     if(a[i] > max) {
       max = a[i];
       max_pos = i;
-    }
+    }i++;
   }
   a[n] += a[max_pos];
   a[max_pos] = a[n] - a[max_pos];
